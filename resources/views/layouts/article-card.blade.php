bfe15dc455b1be8e4c9b7e821031bcc889e20da9
sonhv <90188775+soncon2052003@users.noreply.github.com>
2023-05-17 22:24:08 +0700

    update upload and control image

--- resources/views/layouts/article-card.blade.php
+++ resources/views/layouts/article-card.blade.php
@@ -1,23 +1,23 @@
-<div class="panel panel-primary">
-  <div class="panel-heading"> 
-    <h3 class="panel-title">{{ $title }}</h3>
+<div class="card">
+  <div class="card-header">
+    <h3>{{ $title }}</h3> 
   </div>
-  <div class="panel-body">
+  <div class="card-body">
     <img src="{{ $image }}" width="120px" height="80" class="center"/>
     <div class="h4 text text-primary" style="text-align:center;">{{ $category }}</div>
     <div>{{ $description }}</div>
     <div class="text text-muted" style="text-align:right;">{{ $author}}</div>
   </div>
-  <div class="panel-footer">
+  <div class="card-footer">
     <div class="row">
       <div class="col-md-3">
-        <a href="{{ route('articles.edit', $news->id) }}" class="btn btn-muted">Sửa</a>
+        <a href="{{ route('articles.edit', $news->id) }}" class="btn btn-primary">Sửa</a>
       </div>
       <div class="col-md-3">
         <form action="{{ route('articles.destroy', $news->id) }}" method="POST">
           @csrf
           @method('DELETE')
-          <button type="submit" class="btn btn-muted"><a>Xóa</a></button>
+          <button type="submit" class="btn btn-danger"><a>Xóa</a></button>
         </form>
       </div>
       <div class="col-md-6">
@@ -25,4 +25,4 @@
       </div>
     </div>
   </div>
-</div> 
+</div>
